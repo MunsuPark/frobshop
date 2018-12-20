73cf19683a421a189dd0a7a9d231030042673804
MunsuPark <aspiration@kaist.ac.kr>
2018-12-20 17:48:16 +0900

    ADD: django oscar 기본 세팅.

---
+++ frobshop/urls.py
@@ -0,0 +1,26 @@
+"""frobshop URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include
+from django.urls import path
+from oscar.app import application
+
+urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),  # > Django-2.0
+    path('admin/', admin.site.urls),  # > Django-2.0
+
+    path('', application.urls),  # > Django-2.0
+]
